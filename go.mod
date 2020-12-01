ad0edb4fef17ef133947566db1fd00093a15f712
IVANDI DJOH GAH <ivandi@dbo.id>
2020-12-02 01:30:58 +0700

    Package Updated

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ivandi1980/GoRestApi
 
 go 1.15
 
-require github.com/gofiber/fiber/v2 v2.2.2 // indirect
+require github.com/gofiber/fiber/v2 v2.2.2
--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	Id	int `json:"id"`
+	Name string `json:"name"`
+	Completed bool `json:"completed"`
+}
+
+var todos = []Todo {
+	{
+		Id: 1,
+		Name: "Arjuna",
+		Completed: false,
+	},
+}
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Jiah, Go!!")
+		return c.SendString("Lagi Belajar, Go!!")
 	})
 
-	app.Listen(":3000")
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
